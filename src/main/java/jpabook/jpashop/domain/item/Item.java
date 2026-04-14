77dfd75ffc430a55cc15c659496aa73d8ad8a274
jaewoo-rain <202017499@jbnu.ac.kr>
2026-04-14 18:52:00 +0900

    엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계 전략
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
