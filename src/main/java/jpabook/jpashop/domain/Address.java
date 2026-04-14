572aa6bc9b34cd0fe0c48d5df76f6aa5500e1aa5
jaewoo <202017499@jbnu.ac.kr>
2026-04-14 23:33:24 +0900

    엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,15 @@ import lombok.Getter;
 @Embeddable
 @Getter
 public class Address {
-
     private String city;
     private String street;
     private String zipcode;
 
+    protected Address() {} // JPA용
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -27,7 +27,7 @@ public class Order {
     @OneToMany(cascade = ALL, mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(cascade = ALL, fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,4 +35,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    // == 연관관계 메서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrder().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
