a4c2302bb55f734845608fc49d0767bee5ffaf19
jaewoo <202017499@jbnu.ac.kr>
2026-04-16 22:38:14 +0900

    feat:테스트 코드 작성중

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+}
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+        Address address = new Address("도시", "동", "코드");
+        member.setAddress(address);
+
+        // when
+        Long savedMemberId = memberService.join(member);
+        Member findMember = memberRepository.findOne(savedMemberId);
+
+        // then
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember.getAddress()).isEqualTo(member.getAddress());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        // given
+        Member member1 = new Member();
+        member1.setName("member1");
+
+        Member member2 = new Member();
+        member2.setName("member1");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+    }
+
+}
